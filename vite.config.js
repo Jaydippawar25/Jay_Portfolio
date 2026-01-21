845f4596175b244d2fc157f4040a0dc5d9d10a69
Jaydippawar25 <jaydippawar2512@gmail.com>
2026-01-21 11:45:43 +0530

    updated js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Jay_Portfolio",
+  base: "/Jay_Portfolio/",
   plugins: [react(), tailwindcss()],
 });
